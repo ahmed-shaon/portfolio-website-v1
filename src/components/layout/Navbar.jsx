13a204e10677fa15870159134f95b69241194c1b
unknown <ahmedtanvir444@gmail.com>
2026-01-20 12:28:20 +0600

    implement custom hooks to scroll

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import {Code, Menue, X} from "lucide-react";
+import {NAV_LINKS, PERSONAL_INFO} from "../../utils/constant";
 
 const Navbar = () => {
+  const [isMenueOpen, setIsMenueOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const activeSection = useScrollSpy(NAV_LINKS.map(link => link.id));
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
   return (
     <div>Navbar</div>
   )
