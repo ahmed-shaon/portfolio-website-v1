2818a4ee444aa9c6a4541fc22fdf72bf194ad9f4
unknown <ahmedtanvir444@gmail.com>
2026-01-25 18:06:10 +0600

    implement projects section

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
         id:1, 
         title: "Project One",
         description: "This is the first project.",
-        image:"/images/project1.png",
+        image:"/projects/project1.png",
         category:"Full Stack",
         technologies: ["React", "Node.js", "MongoDB"],
         metrics: '40% increase in user engagement',
@@ -15,7 +15,7 @@ export const projects = [
         id:2, 
         title: "Project Two",
         description: "This is the second project.",
-        image:"/images/project2.png",
+        image:"/projects/project1.png",
         category:"Frontend",
         technologies: ["React", "Tailwind CSS"],
         metrics: '30% increase in user engagement',
@@ -26,7 +26,7 @@ export const projects = [
         id:3, 
         title: "Project Three",
         description: "This is the third project.",
-        image:"/images/project3.png",
+        image:"/projects/project1.png",
         category:"Backend",
         technologies: ["Node.js", "Express", "MongoDB"],
         metrics: '40% increase in user engagement',
@@ -38,7 +38,31 @@ export const projects = [
         id:4, 
         title: "Project Four",
         description: "This is the fourth project.",
-        image:"/images/project4.png",
+        image:"/projects/project1.png",
+        category:"Web Apps, UI Components, ",
+        technologies: ["React", "Node.js", "MongoDB"],
+        metrics: '40% increase in user engagement',
+        githubUrl: "demolink",
+        demoUrl: "demolink"
+
+    },
+    {
+        id:5, 
+        title: "Project Five",
+        description: "This is the fifth project.",
+        image:"/projects/project1.png",
+        category:"Web Apps, UI Components, ",
+        technologies: ["React", "Node.js", "MongoDB"],
+        metrics: '40% increase in user engagement',
+        githubUrl: "demolink",
+        demoUrl: "demolink"
+
+    },
+    {
+        id:6, 
+        title: "Project Six",
+        description: "This is the sixth project.",
+        image:"/projects/project1.png",
         category:"Web Apps, UI Components, ",
         technologies: ["React", "Node.js", "MongoDB"],
         metrics: '40% increase in user engagement',
@@ -49,4 +73,4 @@ export const projects = [
 
 ]
 
-export const categories = ["All","Full Stack", "Web Apps", "UI Components", "Frontend", "Backend"]
+export const categories = ["All","Full Stack", "Web Apps", "UI Components"]
